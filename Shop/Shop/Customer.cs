97a928bef3e20a0f7069584fd718413845206f4f
郑少朋 <911917@nd>
2019-11-07 14:31:18 +0800

    2、提取费用计算

--- Shop/Shop/Customer.cs
+++ Shop/Shop/Customer.cs
@@ -25,37 +25,12 @@ namespace Ch01
             int frequentRenterPoints = 0;
             TextWriter textWriter = new StringWriter();
             textWriter.WriteLine("租借人：" + Name);
-            for (int index = 0; index < _rentals.Count; index++)
+            foreach (Rental rental in _rentals)
             {
                 double thisAmount = 0;
-                Rental each = _rentals[index];
+                Rental each = rental;
 
-                switch (each.Movie.PriceCode)
-                {
-                    case Movie.Regular:
-                    {
-                        thisAmount += 2;
-                        if (each.DayRented>2)
-                        {
-                            thisAmount += (each.DayRented - 2) * 1.5;
-                        }
-                    }
-                        break;
-                    case Movie.NewRelease:
-                    {
-                        thisAmount += each.DayRented * 3;
-                    }
-                        break;
-                    case Movie.Children:
-                    {
-                        thisAmount += 1.5;
-                        if (each.DayRented>3)
-                        {
-                            thisAmount += (each.DayRented - 3) * 1.5;
-                        }
-                    }
-                        break;
-                }
+                thisAmount = GetRentalAmount(each);
 
                 //add frequentRenterPoints
                 frequentRenterPoints++;
@@ -72,6 +47,38 @@ namespace Ch01
             textWriter.WriteLine("本次费用合计： " + totalAmount);
             textWriter.WriteLine("累计获得" + frequentRenterPoints + " 积分。");
             return textWriter.ToString();
+        }
+
+        private static double GetRentalAmount(Rental rental)
+        {
+            double thisAmount = 0;
+            switch (rental.Movie.PriceCode)
+            {
+                case Movie.Regular:
+                {
+                    thisAmount += 2;
+                    if (rental.DayRented > 2)
+                    {
+                        thisAmount += (rental.DayRented - 2) * 1.5;
+                    }
+                }
+                    break;
+                case Movie.NewRelease:
+                {
+                    thisAmount += rental.DayRented * 3;
+                }
+                    break;
+                case Movie.Children:
+                {
+                    thisAmount += 1.5;
+                    if (rental.DayRented > 3)
+                    {
+                        thisAmount += (rental.DayRented - 3) * 1.5;
+                    }
+                }
+                    break;
+            }
+            return thisAmount;
         }
     }
 }
