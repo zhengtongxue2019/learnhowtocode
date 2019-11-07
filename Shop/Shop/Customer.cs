5349a782b6bd11548b086643db963b5bfc917166
郑少朋 <911917@nd>
2019-11-07 14:55:33 +0800

    7、观察提取后的函数，发现有些函数的参数是另外一个类的对象，考虑把相关的函数移到对应的类中。采用去除静态函数的方法来重构实现。

--- Shop/Shop/Customer.cs
+++ Shop/Shop/Customer.cs
@@ -23,10 +23,11 @@ namespace Ch01
         {
             TextWriter textWriter = new StringWriter();
             textWriter.WriteLine("租借人：" + Name);
+
             foreach (Rental rental in _rentals)
             {
                 //show figures for this rental
-                textWriter.WriteLine(rental.Movie.Title + ":" + GetRentalAmount(rental));
+                textWriter.WriteLine(rental.Movie.Title + ":" + rental.GetRentalAmount());
             }
 
             textWriter.WriteLine("本次费用合计： " + GetTotalAmount());
@@ -39,7 +40,7 @@ namespace Ch01
             int frequentRenterPoints = 0;
             foreach (Rental rental in _rentals)
             {
-                frequentRenterPoints += GetFrequentRenterPoints(rental);
+                frequentRenterPoints += rental.GetFrequentRenterPoints();
             }
             return frequentRenterPoints;
         }
@@ -49,53 +50,9 @@ namespace Ch01
             double totalAmount = 0;
             foreach (Rental rental in _rentals)
             {
-                totalAmount += GetRentalAmount(rental);
+                totalAmount += rental.GetRentalAmount();
             }
             return totalAmount;
-        }
-
-        private static int GetFrequentRenterPoints(Rental rental)
-        {
-            int frequentRenterPoints = 0;
-            //add frequentRenterPoints
-            frequentRenterPoints++;
-            if (rental.Movie.PriceCode == Movie.NewRelease && rental.DayRented > 1)
-            {
-                frequentRenterPoints++;
-            }
-            return frequentRenterPoints;
-        }
-
-        private static double GetRentalAmount(Rental rental)
-        {
-            double thisAmount = 0;
-            switch (rental.Movie.PriceCode)
-            {
-                case Movie.Regular:
-                    {
-                        thisAmount += 2;
-                        if (rental.DayRented > 2)
-                        {
-                            thisAmount += (rental.DayRented - 2) * 1.5;
-                        }
-                    }
-                    break;
-                case Movie.NewRelease:
-                    {
-                        thisAmount += rental.DayRented * 3;
-                    }
-                    break;
-                case Movie.Children:
-                    {
-                        thisAmount += 1.5;
-                        if (rental.DayRented > 3)
-                        {
-                            thisAmount += (rental.DayRented - 3) * 1.5;
-                        }
-                    }
-                    break;
-            }
-            return thisAmount;
         }
     }
 }
--- Shop/Shop/Rental.cs
+++ Shop/Shop/Rental.cs
@@ -11,5 +11,49 @@
         public int DayRented { get; }
 
         public Movie Movie { get; }
+
+        public int GetFrequentRenterPoints()
+        {
+            int frequentRenterPoints = 0;
+            //add frequentRenterPoints
+            frequentRenterPoints++;
+            if (Movie.PriceCode == Movie.NewRelease && DayRented > 1)
+            {
+                frequentRenterPoints++;
+            }
+            return frequentRenterPoints;
+        }
+
+        public double GetRentalAmount()
+        {
+            double thisAmount = 0;
+            switch (Movie.PriceCode)
+            {
+                case Movie.Regular:
+                {
+                    thisAmount += 2;
+                    if (DayRented > 2)
+                    {
+                        thisAmount += (DayRented - 2) * 1.5;
+                    }
+                }
+                    break;
+                case Movie.NewRelease:
+                {
+                    thisAmount += DayRented * 3;
+                }
+                    break;
+                case Movie.Children:
+                {
+                    thisAmount += 1.5;
+                    if (DayRented > 3)
+                    {
+                        thisAmount += (DayRented - 3) * 1.5;
+                    }
+                }
+                    break;
+            }
+            return thisAmount;
+        }
     }
 }
