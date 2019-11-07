644ecd58664be75b5f7f0dc84609c5aca1c10381
郑少朋 <911917@nd>
2019-11-07 15:25:29 +0800

    10、观察Movie类发现switch依赖的是PriceCode来变化，是否可以考虑封装PriceCode到一个Price类中来实现多态化。

--- Shop/Shop/Movie.cs
+++ Shop/Shop/Movie.cs
@@ -1,4 +1,6 @@
-﻿namespace Ch01
+﻿using System;
+
+namespace Ch01
 {
     internal class Movie
     {
@@ -6,15 +8,43 @@
         public const int Regular = 0;//普通片
         public const int NewRelease = 1;//新片
 
+        private Price _price;
+
         public Movie(string title, int priceCode)
         {
             Title = title;
+
+            switch (priceCode)
+            {
+                case Movie.Regular:
+                    {
+                        _price = new RegularPrice();
+                    }
+                    break;
+                case Movie.NewRelease:
+                    {
+                        _price = new NewReleasePrice();
+                    }
+                    break;
+                case Movie.Children:
+                    {
+                        _price = new ChildrenPrice();
+                    }
+                    break;
+                default:
+                    throw new ArgumentException("没有找到对应的类别");
+            }
+
             PriceCode = priceCode;
         }
 
         public string Title { get; }
 
-        public int PriceCode { get; set; }
+        public int PriceCode
+        {
+            get { return _price.GetPriceCode(); }
+            set { _price.SetPriceCode(value); }
+        }
 
         public int GetFrequentRenterPoints(int DayRented)
         {
@@ -34,27 +64,27 @@
             switch (PriceCode)
             {
                 case Movie.Regular:
-                {
-                    thisAmount += 2;
-                    if (DayRented > 2)
                     {
-                        thisAmount += (DayRented - 2) * 1.5;
+                        thisAmount += 2;
+                        if (DayRented > 2)
+                        {
+                            thisAmount += (DayRented - 2) * 1.5;
+                        }
                     }
-                }
                     break;
                 case Movie.NewRelease:
-                {
-                    thisAmount += DayRented * 3;
-                }
+                    {
+                        thisAmount += DayRented * 3;
+                    }
                     break;
                 case Movie.Children:
-                {
-                    thisAmount += 1.5;
-                    if (DayRented > 3)
                     {
-                        thisAmount += (DayRented - 3) * 1.5;
+                        thisAmount += 1.5;
+                        if (DayRented > 3)
+                        {
+                            thisAmount += (DayRented - 3) * 1.5;
+                        }
                     }
-                }
                     break;
             }
             return thisAmount;
---
+++ Shop/Shop/Price.cs
@@ -0,0 +1,39 @@
+﻿namespace Ch01
+{
+    public abstract class Price
+    {
+        private int _priceCode;
+
+        public abstract int GetPriceCode();
+
+        public void SetPriceCode(int priceCode)
+        {
+            _priceCode = priceCode;
+        }
+    }
+
+    public class ChildrenPrice : Price
+    {
+        public override int GetPriceCode()
+        {
+            return Movie.Children;
+        }
+    }
+
+    public class RegularPrice : Price
+    {
+        public override int GetPriceCode()
+        {
+            return Movie.Regular;
+        }
+    }
+
+    public class NewReleasePrice : Price
+    {
+        public override int GetPriceCode()
+        {
+            return Movie.NewRelease;
+        }
+    }
+
+}
