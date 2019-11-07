94f24bc9cad77fbcedbd346ccfdfe4eebe8f3f04
郑少朋 <911917@nd>
2019-11-07 15:08:04 +0800

    9、观察Rental发现switch依赖的是Movie中的变量，所以考虑把方法进一步迁移到Movie中去。 手动迁移，先拷贝方法到Movie中，再在Rental中调用该方法，测试通过后删除不需要的代码。

--- Shop/Shop/Movie.cs
+++ Shop/Shop/Movie.cs
@@ -15,5 +15,49 @@
         public string Title { get; }
 
         public int PriceCode { get; set; }
+
+        public int GetFrequentRenterPoints(int DayRented)
+        {
+            int frequentRenterPoints = 0;
+            //add frequentRenterPoints
+            frequentRenterPoints++;
+            if (PriceCode == Movie.NewRelease && DayRented > 1)
+            {
+                frequentRenterPoints++;
+            }
+            return frequentRenterPoints;
+        }
+
+        public double GetRentalAmount(int DayRented)
+        {
+            double thisAmount = 0;
+            switch (PriceCode)
+            {
+                case Movie.Regular:
+                {
+                    thisAmount += 2;
+                    if (DayRented > 2)
+                    {
+                        thisAmount += (DayRented - 2) * 1.5;
+                    }
+                }
+                    break;
+                case Movie.NewRelease:
+                {
+                    thisAmount += DayRented * 3;
+                }
+                    break;
+                case Movie.Children:
+                {
+                    thisAmount += 1.5;
+                    if (DayRented > 3)
+                    {
+                        thisAmount += (DayRented - 3) * 1.5;
+                    }
+                }
+                    break;
+            }
+            return thisAmount;
+        }
     }
 }
